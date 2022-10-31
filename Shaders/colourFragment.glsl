eafc334683ba8bf7854487325eb7e63863fa3061
b9032016 <b.summers1@newcastle.ac.uk>
2022-10-31 11:54:37 +0000

    Completed the Tutorial 1 shader challenge as well as the code for tutorial 8.

--- Shaders/colourFragment.glsl
+++ Shaders/colourFragment.glsl
@@ -7,5 +7,14 @@ in Vertex{
 out vec4 fragColour;
 
 void main(void){
+    //if((IN.colour.r > 0.1 && IN.colour.r < 0.9) &&
+    //(IN.colour.g > 0.1 && IN.colour.g < 0.9) &&
+    //(IN.colour.b > 0.1 && IN.colour.b < 0.9)){
+        //discard;
+    //}
+    if(IN.colour.r < 0.5 && IN.colour.g < 0.5 && IN.colour.b < 0.5){
+        discard;
+    }
+
     fragColour = IN.colour;
 }
